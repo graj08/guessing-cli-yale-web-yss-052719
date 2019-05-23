8c62a6e5e743a626383c936342271de03cbbd48f
gorgeous-karma-7335 <graj08@gmail.com>
2019-05-23 21:34:48 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,7 +1,8 @@
 def run_guessing_game
-true_value = rand(1..100)
-puts "input your guess, between 1 and 100:"
+while user_guess != "exit"
+puts "Guess a number between 1 and 6."
 user_guess = gets.chomp
+true_value = rand(1..6)
 if user_guess == true_value
   puts "You guessed the correct number!"
 else
